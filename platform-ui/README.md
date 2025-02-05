de7f1442edf59f53890bfed28bab3a0dfffea0d9
Ajay <ajay.choudhary@simadvisorypartner.com>
2025-02-05 17:15:40 +0530

    added platform-ui library

---
+++ platform-ui/README.md
@@ -0,0 +1,3 @@
+# platform-ui
+
+This library was generated with [Nx](https://nx.dev).
---
+++ platform-ui/src/lib/platform-ui/platform-ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-platform-ui',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './platform-ui.component.html',
+  styleUrl: './platform-ui.component.css',
+})
+export class PlatformUiComponent {}
